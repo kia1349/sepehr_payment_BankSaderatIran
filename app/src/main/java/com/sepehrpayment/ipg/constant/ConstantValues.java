d79c7de19e6e3b48f25db42e9e6e515e9011e4a4
kia1349 <kia1349@yahoo.com>
2021-10-06 12:33:17 +0330

    sepehr payment gateway(bank saderat iran)

--- app/src/main/java/com/sepehrpayment/ipg/constant/ConstantValues.java
+++ app/src/main/java/com/sepehrpayment/ipg/constant/ConstantValues.java
@@ -13,6 +13,5 @@ public class ConstantValues {
     public static String MESSAGE_USER_PAID_SEPEHRPAY = "";
     //    public static String MESSAGE_USER_PAID_SEPEHRPAY_WEB_VIEW = "";
     public static String SEPEHRPAY_PUBLISHABLE_KEY = "61000063";
-    public static String LANGUAGE_DIRECTION = "rtl";
 
 }
